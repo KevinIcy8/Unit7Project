8b89259a2ce5d89f182e60be83c12cd193bf644a
BT_4N2_23 <KevinIcy8>
2023-02-28 12:35:56 -0500

    added stuff

--- src/PackageSimulator.java
+++ src/PackageSimulator.java
@@ -2,4 +2,37 @@ import java.util.ArrayList;
 
 public class PackageSimulator {
     private ArrayList<Package> packages;
+
+   /* public ArrayList<Package> generatePackages(int numOfPackages){
+        for(int i = 0; i < numOfPackages; i++){
+            packages.add(new Package());
+        }
+        return packages;
+    }
+
+   /* public Address addressGenerator(){
+        String[] states = {"Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware",
+                "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland",
+                "Massachusetts, Michigan, Minnesota, Mississippi, Missouri, Montana, Nebraska, Nevada, New Hampshire, " +
+                "New Jersey, New Mexico, New York, North Carolina, North Dakota, Ohio, Oklahoma, Oregon, Pennsylvania, " +
+                "Rhode Island, South Carolina, South Dakota, Tennessee, Texas, Utah, Vermont, Virginia, Washington," +
+                " West Virginia, Wisconsin, Wyoming"};
+
+        //Address random = new Address()
+
+    }
+
+    */
+
+    public double generateTotalCost(){
+        return 0.0;
+    }
+
+    public String getSimulationInfo(){
+        return "";
+    }
+
+    public void resetSimulation(){
+
+    }
 }
